63daf6c95414b91708561b80f65c65b1f5256c41
CoolTonie <nwachukwua99@gmail.com>
2022-06-16 15:01:19 +0100

    final push after making post the only class

--- I4G009482AEH/blog/admin.py
+++ I4G009482AEH/blog/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Text,Title,Author,Created_date,Published_date
+from .models import post
 # Register your models here.
 
-admin.site.register(Text)
-admin.site.register(Title)
-admin.site.register(Author)
-admin.site.register(Created_date)
-admin.site.register(Published_date)
+admin.site.register(post)
+
--- I4G009482AEH/blog/models.py
+++ I4G009482AEH/blog/models.py
@@ -10,36 +10,12 @@ from django.contrib.auth import get_user_model
 from django.utils.timezone import timezone
 
 # Create your models here.
-# Post
-
-# --------
-
-# Title : A string of maxlength 200, use Django’s models.CharField
-class Title(models.Model):
+class post(models.Model):
     title=models.CharField(max_length=200)
-    author=models.CharField(max_length=200)
-    contents=models.CharField(max_length=500)
-    
- 
-# Text : Any amount of text, use Django’s TextField
-class Text(models.Model):
-    body=models.CharField(max_length=2000)
-     
-# Author : A Foreign Key to the current user model. Make use of Django’s get_user_model function.
-class Author(models.Model):
-    name=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    first_name= models.CharField(max_length=50)
-    last_name= models.CharField(max_length=50)
- 
-# Created_date : A date-time column, use Django’s models.DateTimeField. 
-class Created_date(models.Model):
-    date=models.DateTimeField(default=datetime.date)
-    time=models.DateTimeField(auto_now=True)
- 
-# Published_date : A date-time column, use Django’s models.DateTimeField. 
-class Published_date(models.Model):
-    date=models.DateTimeField(default=datetime.date)
-    time=models.DateTimeField(auto_now=True)
+    Text=models.CharField(max_length=2000)
+    Author=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_date=models.DateTimeField(auto_now=True)
+    Published_date=models.DateTimeField(auto_now=True)
 
 
 
